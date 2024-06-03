3636a496baf50111ec4cc23bce4ea69f15469d4f
drashti-panchal <drashtipanchal112@gmail.com>
2024-06-03 08:45:07 +0530

    Memory storage

---
+++ 6_Stack_Heap.js
@@ -0,0 +1,21 @@
+//Stack(Stores Primitive Datatype) 
+//Heap(Stores Non Primitive Datatype)
+
+//Stack
+let name="Drashti"
+let name2 = name
+name2="Parth"
+console.log(name2)
+console.log(name)
+
+//Heap
+let obj ={
+	name:"Drashti",
+	age:18
+}
+
+let obj2=obj
+obj2.name="Parth"
+obj2.age=24
+console.log(obj.name)
+console.log(obj.age)

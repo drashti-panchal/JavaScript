65718d02d82b11d5140c35ccbaaa29c3df0c3896
drashti-panchal <drashtipanchal112@gmail.com>
2024-07-03 11:22:35 +0530

    Relational Operations

--- 4_Comparison.js
+++ 4_Comparison.js
@@ -8,6 +8,9 @@ console.log(2 != 1);
 console.log("2" > 1);
 console.log("02" > 1);
 
+
+// It gives true because comparison and equality operator works differently. Comparison 
+// operator converts null to 0 whereas equality operator doesnt.
 console.log(null > 0); //False
 console.log(null == 0);//False
 console.log(null >= 0);//True

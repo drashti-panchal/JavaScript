763691469693ce5ec3224a6f2fcbb12cabe1fa1e
drashti-panchal <drashtipanchal112@gmail.com>
2024-07-03 10:20:56 +0530

    Updated

--- 1_Variables.js
+++ 1_Variables.js
@@ -1,10 +1,14 @@
 const accountId="12345"
 let accountEmail="drashtipanchal112@gmail.com"
 var account ="Blah"
+let accountState;
 
 console.log(accountId)
 
-console.table([accountId, accountEmail, account])
+console.table([accountId, accountEmail, account,accountState])
+
+
+
 
 
 
